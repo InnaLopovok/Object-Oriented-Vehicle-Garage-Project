f14e6ce7b44838c3349b73bcd25566e5d598556c
InnaLopovok <101043653+InnaLopovok@users.noreply.github.com>
2023-01-05 13:12:00 +0200

    Add files via upload

---
+++ Project.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex03.ConsoleUI
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Ex3_Inna_Adam;
+
+    class Project
+    {
+        public static void Main(string[] args) 
+        {
+            UserInterface ui = new UserInterface();
+            ui.GarageManagement();
+        }
+    }
+}
